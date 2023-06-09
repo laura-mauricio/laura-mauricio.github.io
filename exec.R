aa1693376abb3265027365a76ee2536604d656eb
yurilavinas <yclavinas@gmail.com>
2023-06-09 17:14:21 -0300

    adding the basic structure

---
+++ exec.R
@@ -0,0 +1,11 @@
+#Setup
+library(rmarkdown)
+# library(render_site)
+library(dplyr)
+
+setwd("~/Documents/laura-mauricio.github.io/")
+knitr::opts_chunk$set()
+
+#Execute
+render_site()
+# beep()  #finished rendering
